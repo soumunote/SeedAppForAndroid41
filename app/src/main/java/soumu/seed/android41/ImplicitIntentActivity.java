ad97b3e1426f49a835af41498859b736483516a3
前田　良一 <maeda@s-max.co.jp>
2016-04-04 21:44:29 +0900

    04/04

---
+++ app/src/main/java/soumu/seed/android41/ImplicitIntentActivity.java
@@ -0,0 +1,13 @@
+package soumu.seed.android41;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImplicitIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intent);
+    }
+}
--- app/src/main/java/soumu/seed/android41/LifecycleActivity.java
+++ app/src/main/java/soumu/seed/android41/LifecycleActivity.java
@@ -1,18 +1,26 @@
 package soumu.seed.android41;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
- *
  * ライフサイクルメソッドの動作を確認します
  * AndroidStudio の 「6.Android Monitor」の「logcat」タブで確認する
- *
  */
 public class LifecycleActivity extends AppCompatActivity {
 
     private String TAG;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     {
         TAG = this.getClass().getName();
@@ -22,12 +30,47 @@ public class LifecycleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifecycle);
-        Log.d(TAG, "%m"+ "AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, getCurrentMethodName());
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onStart");
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, getCurrentMethodName());
+    }
+
+    private String getCurrentMethodName() {
+        StackTraceElement[] s = Thread.currentThread().getStackTrace();
+        return s[3].getMethodName();
     }
 }
