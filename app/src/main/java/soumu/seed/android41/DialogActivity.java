69517475f174472329889a06dea817178bcbdd63
maeda <soumunote@gmail.com>
2016-04-09 14:42:16 +0900

    BetterDialog 作成中

--- app/src/main/java/soumu/seed/android41/DialogActivity.java
+++ app/src/main/java/soumu/seed/android41/DialogActivity.java
@@ -1,15 +1,19 @@
 package soumu.seed.android41;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class DialogActivity extends AppCompatActivity {
+
+    private final static int DIALOG_ID_1 = 1;
 
     private String TAG;
     private Toast toast = null;
@@ -22,6 +26,7 @@ public class DialogActivity extends AppCompatActivity {
         TAG = this.getClass().getName();
 
         Button btnMultiSelect = (Button)findViewById(R.id.btnMultiSelect);
+        assert btnMultiSelect != null;
         btnMultiSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,5 +66,34 @@ public class DialogActivity extends AppCompatActivity {
             }
         });
 
+        Button btnBetterMethod = (Button)findViewById(R.id.btnBetterMethod);
+        btnBetterMethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID_1);
+            }
+        });
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DIALOG_ID_1:
+                LayoutInflater factory = LayoutInflater.from(this);
+                final View inputView = factory.inflate(R.layout.activity_dialog_custom_dialog_1, null);
+                return new AlertDialog.Builder(DialogActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("BetterDialog")
+                        .setView(inputView)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).create();
+
+        }
+        return null;
     }
 }
